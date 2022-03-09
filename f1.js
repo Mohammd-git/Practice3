7d8ab66ab4b8eae90a6113699a9cb91fb416c343
Mohammd-git <cdrmohammad@gmail.com>
2022-03-09 23:25:13 +0530

    w1 added

--- f1.js
+++ f1.js
@@ -1,5 +1,10 @@
 // the hello world program
 console.log('Hello ');
 console.log('World');
+console.log('World1'); 
+// console.log('World2');
+// console.log('World3');
+// console.log('World4');
+// console.log('World5');
 
 
