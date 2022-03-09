ca00560f042ae5deb199d05e9eb7fc4929b46faa
Mohammd-git <cdrmohammad@gmail.com>
2022-03-09 23:26:00 +0530

    w2 added

--- f1.js
+++ f1.js
@@ -2,7 +2,7 @@
 console.log('Hello ');
 console.log('World');
 console.log('World1'); 
-// console.log('World2');
+console.log('World2');
 // console.log('World3');
 // console.log('World4');
 // console.log('World5');
